b1215df0d10adf62e8e534cb6c2fbec2eb1aea13
Vladimir Kolovski <vladimir.kolovski@databricks.com>
2022-06-09 21:13:01 -0700

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,2 +1,6 @@
-# Databricks notebook source
-print("Hello World")
+import mlflow
+
+run = mlflow.start_run()
+run_id = mlflow.active_run().info.run_id
+mlflow.end_run()
+dbutils.notebook.exit(run_id)
